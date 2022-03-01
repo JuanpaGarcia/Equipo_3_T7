9495d5f8cb400414defb0994db15c150764c4e49
ie718817 <ie718817@iteso.mx>
2022-03-01 09:11:02 -0600

    Final version

--- main.c
+++ main.c
@@ -35,8 +35,8 @@ const State_t FSM[4] =
 		{{blue, green, white}, PIT_0, PIT_callback_init, {No_Color, Secuence_1, Secuence2, Secuence_3}}
 };
 
-void init_interrupt(void);
-void init(void);
+void init_interrupt();
+void init();
 
 
 int main()
@@ -44,9 +44,9 @@ int main()
 	init();
 	init_interrupt();
 
-	uint8_t state_t = No_Color;
-	uint8_t turn_t = 0;
-	uint8_t sw_input_t = Not_Pressed;
+	uint8_t state = No_Color;
+	uint8_t turn = 0;
+	uint8_t sw_input = Not_Pressed;
 	void (*ftpr_selected_color)(void) = 0;
 	PIT_delay(PIT_0, SYSTEM_CLOCK, DELAY_TIME);
 
@@ -54,49 +54,45 @@ int main()
 	while(TRUE)
 	{
 
-		if (Color_Quantity_Per_Funciton <= turn_t)	turn_t = 0; //Ensure not accesing an outbound value in Color Matrix
+		if (Color_Quantity_Per_Funciton <= turn) turn = 0; //Ensure not accesing an outbound value in Color Matrix
+		ftpr_selected_color = FSM[state].ColorMatrix[turn];
+		FSM[state].fptrcallback(FSM[state].Timer, ftpr_selected_color);
 
-		ftpr_selected_color = FSM[state_t].ColorMatrix[turn_t];
-		FSM[state_t].fptrcallback(FSM[state_t].Timer, ftpr_selected_color);
-
-		if(GPIO_get_irq_status(GPIO_C))
-		{
-			if(GPIO_get_irq_status(GPIO_A))
-			{
- 				sw_input_t = SW2_SW3;
-				turn_t = 0;									//Resets count
+		if(GPIO_get_irq_status(GPIO_C)){
+			if(BIT_OFF == GPIO_read_pin(GPIO_A, 4)){
+ 				sw_input = SW2_SW3;
+				turn = 0;									//Resets count
 				GPIO_clear_irq_status(GPIO_A);
 				GPIO_clear_irq_status(GPIO_B);
-			}
-			else
-			{
-				sw_input_t = SW2;
-				turn_t = 0;									//Resets count
+			}else{
+				sw_input = SW2;
+				turn = 0;									//Resets count
 				GPIO_clear_irq_status(GPIO_C);
 			}
-		}
-		else
-		{
-			if(GPIO_get_irq_status(GPIO_A))
-			{
-				sw_input_t = SW3;
-				turn_t = 0;									//Resets count
+		}else{
+			if(GPIO_get_irq_status(GPIO_A)){
 				GPIO_clear_irq_status(GPIO_A);
+				if(BIT_OFF == GPIO_read_pin(GPIO_C, 6)){
+
+				}else{
+					sw_input = SW3;
+					turn = 0;								//Resets count
+				}
 			}
 		}
 
-		state_t = FSM[state_t].next[sw_input_t];
+		state = FSM[state].next[sw_input];
 
 		if(PIT_get_interrupt_flag_status())
 		{
-			turn_t++;
+			turn++;
 			PIT_clear_interrupt_flag();
 		}
 	}
 	return 0;
 }
 
-void init(void)
+void init()
 {
 	gpio_pin_control_register_t input_intr_config = GPIO_MUX1|GPIO_PE|GPIO_PS|INTR_FALLING_EDGE; // SW interrupt config
 	gpio_pin_control_register_t pcr_gpioe_pin_led = GPIO_MUX1;
@@ -130,16 +126,16 @@ void init(void)
 
 
 
-void init_interrupt(void)
+void init_interrupt()
 {
 
-	/*Sets the threshold for interrupts, if the interrupt has higher priority constant that the BASEPRI, the interrupt will not be attended*/
+	/**Sets the threshold for interrupts, if the interrupt has higher priority constant that the BASEPRI, the interrupt will not be attended*/
 	NVIC_set_basepri_threshold(PRIORITY_10);
-	/*Enables and sets a particular interrupt and its priority*/
+	/**Enables and sets a particular interrupt and its priority*/
 	NVIC_enable_interrupt_and_priotity(PIT_CH0_IRQ, PRIORITY_3);
-	/*Enables and sets a particular interrupt and its priority*/
+	/**Enables and sets a particular interrupt and its priority*/
 	NVIC_enable_interrupt_and_priotity(PORTC_IRQ, PRIORITY_4);
-	/*Enables and sets a particular interrupt and its priority*/
+	/**Enables and sets a particular interrupt and its priority*/
 	NVIC_enable_interrupt_and_priotity(PORTA_IRQ, PRIORITY_4);
 
 	NVIC_global_enable_interrupts;
